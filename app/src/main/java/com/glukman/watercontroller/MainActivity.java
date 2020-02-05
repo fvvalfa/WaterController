883352d9fa0c51c487ed5ade028d51ee35c88828
fvvalfa <fvv.alfa@gmail.com>
2020-02-05 22:10:20 +0500

    first commit

---
+++ app/src/main/java/com/glukman/watercontroller/MainActivity.java
@@ -0,0 +1,142 @@
+package com.glukman.watercontroller;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    UdpClientHandler udpClientHandler;
+    UdpClientThread udpClientThread;
+    TextView txtView;
+    Button btn_Send;
+    TextView txtSetIP;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        btn_Send = (Button) findViewById(R.id.btn_send);
+        txtView = (TextView) findViewById(R.id.textView2);
+        btn_Send.setOnClickListener(this);
+        txtSetIP = (TextView) findViewById(R.id.txtSetIP);
+        udpClientHandler = new UdpClientHandler(this);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()) {
+            case R.id.btn_send: txtView.setText("Нажата кнопка Button1");
+                udpClientThread = new UdpClientThread(
+                        txtSetIP.getText().toString(),
+                        Integer.parseInt("11111"),
+                        udpClientHandler);
+                udpClientThread.start();
+
+                btn_Send.setEnabled(false);break;
+
+        }
+    }
+    private void updateState(String state){
+        txtView.setText(state);
+    }
+
+    private void updateRxMsg(String rxmsg){
+        txtView.append(rxmsg + "\n");
+    }
+
+    private void clientEnd(){
+        udpClientThread = null;
+        txtView.setText("clientEnd()");
+        btn_Send.setEnabled(true);
+
+    }
+    public static class UdpClientHandler extends Handler {
+        public static final int UPDATE_STATE = 0;
+        public static final int UPDATE_MSG = 1;
+        public static final int UPDATE_END = 2;
+        private MainActivity parent;
+
+        public UdpClientHandler(MainActivity parent) {
+            super();
+            this.parent = parent;
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+
+            switch (msg.what){
+                case UPDATE_STATE:
+                    parent.updateState((String)msg.obj);
+                    break;
+                case UPDATE_MSG:
+                    parent.updateRxMsg((String)msg.obj);
+                    break;
+                case UPDATE_END:
+                    parent.clientEnd();
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+
+        }
+    }
+}
